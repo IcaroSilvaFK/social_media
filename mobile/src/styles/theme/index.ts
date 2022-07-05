157a7af578d5290cfa4b35dda72f7d798c29ef58
Jeanluca-Moren0 <jeanlucam12@gmail.com>
2022-07-05 19:00:28 -0300

    chore: (adding color theme to project)

--- mobile/src/styles/theme/index.ts
+++ mobile/src/styles/theme/index.ts
@@ -1,5 +1,22 @@
 export const theme = {
-  colors: {},
+  colors: {
+    brand: {
+      100: '#F66EF7',
+      200: '#F66EF7',
+      300: '#A615AB',
+      400: '#800086',
+      500: '#5A0064'
+    },
+    neutrals: {
+      gray: {
+        300: '#BFBFBF',
+        400: '#737373',
+        500: '#595959'
+      },
+      white: '#fff',
+      black: '#000'
+    }
+  },
   sizes: {},
   fonts: {
     poppins: {
