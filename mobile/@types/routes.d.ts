66b6d70f7b3fcef71bb3a751f219d110c7ded69a
Icaro Vieira <icarovsilva1@gmail.com>
2022-07-05 19:44:14 -0300

    feat:(hooks) create hook router

---
+++ mobile/@types/routes.d.ts
@@ -0,0 +1,10 @@
+export declare global {
+  namespace ReactNavigation {
+    interface RootParamList {
+      Login: undefined;
+      Create: undefined;
+      Home: undefined;
+      Profile: undefined;
+    }
+  }
+}
---
+++ mobile/src/hooks/useRouter.ts
@@ -0,0 +1,13 @@
+import { useNavigation } from '@react-navigation/native';
+
+type Routes = 'Login' | 'Create' | 'Home' | 'Profile';
+
+export function useNavigate() {
+  const { navigate } = useNavigation();
+
+  function push(route: Routes) {
+    navigate(route);
+  }
+
+  return push;
+}
