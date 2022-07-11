ba184a51adcc179de4f4eb7c1a0cee4230cf2933
Icaro Vieira <icarovsilva1@gmail.com>
2022-07-11 19:14:37 -0300

    change: remove body-parse middleware :ok_hand:

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -5,8 +5,7 @@ import { imagesRouter, usersRouter, postRouter } from './routes';
 
 const app = express();
 
-app.use(express.urlencoded({ limit: '50mb' }));
-app.use(express.json());
+app.use(express.json({limit: '50mb'}));
 app.use(cors());
 app.use(imagesRouter, usersRouter, postRouter);
 
